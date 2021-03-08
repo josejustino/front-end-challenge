091269a31cd4711c2f0858db1d8476cafa07ded5
José <josejustinno@gmail.com>
2021-03-07 22:52:16 -0300

    Create data list component

--- src/components/DataList/index.tsx
+++ src/components/DataList/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Card } from './styles';
 
-const DataList: React.FC = () => {
-  return <Container />;
+interface MovieProps {
+  id: number;
+  poster_path: string;
+  title: string;
+}
+
+interface PopularMovieProps {
+  popularMovies: Array<MovieProps>;
+}
+
+const DataList: React.FC<PopularMovieProps> = ({ popularMovies }) => {
+  console.log(popularMovies);
+  return (
+    <Container>
+      {popularMovies.map(popularMovie => {
+        return (
+          <Card key={popularMovie.id}>
+            <div>
+              <img
+                src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${popularMovie.poster_path}`}
+                alt={popularMovie.title}
+              />
+            </div>
+          </Card>
+        );
+      })}
+    </Container>
+  );
 };
 
 export default DataList;
