00dacf7d68ecf267900a502a79a9ff9129da33f5
José <josejustinno@gmail.com>
2021-03-07 22:52:30 -0300

    Create data list styles

--- src/components/DataList/styles.ts
+++ src/components/DataList/styles.ts
@@ -1,3 +1,23 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
+
+  margin-top: 2rem;
+`;
+
+export const Card = styled.div`
+  width: 10rem;
+  height: 15rem;
+
+  & > div {
+    img {
+      width: 10rem;
+      height: 15rem;
+
+      border-radius: 10px;
+    }
+  }
+`;
