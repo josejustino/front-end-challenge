f1f9dab2c9ea662c7b4cdad24db7cb264033e324
José <josejustinno@gmail.com>
2021-03-07 22:53:12 -0300

    style header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,19 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
+  flex: 1;
   display: flex;
   flex-direction: column;
+
+  background: var(--header);
+
+  padding: 20px;
+  position: fixed;
+  width: 100%;
+
+  & > div {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
 `;
