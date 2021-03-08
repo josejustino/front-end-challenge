20876009af5940a111979af3fa99fca83d441c66
José <josejustinno@gmail.com>
2021-03-07 22:54:35 -0300

    create home styles

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,29 @@
 import styled from 'styled-components';
 
-export const Container = styled.main``;
+export const Container = styled.div`
+  width: 100%;
+`;
 
-export const Content = styled.div``;
+export const Main = styled.main`
+  width: 100%;
+  display: block;
+`;
+
+export const Content = styled.section`
+  position: fixed;
+  top: 3.8rem;
+  right: 0;
+  bottom: 0;
+  overflow-y: auto;
+  margin: 0 auto;
+  width: 100%;
+`;
+
+export const Section = styled.section`
+  width: min(90vw, 1200px);
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
