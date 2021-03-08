af740fb7274657f6ee356b8ff08332083fbecc7b
José <josejustinno@gmail.com>
2021-03-07 22:54:17 -0300

    Create home page and call api to bring the popular movies

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import api from '../../services/api';
+
+import { PRIVATE_KEY } from '../../config/index';
 
 import Header from '../../components/Header';
 import DataList from '../../components/DataList';
 
-import { Container, Content } from './styles';
+import { Container, Main, Content, Section } from './styles';
+
+interface PopularMovie {
+  id: number;
+  poster_path: string;
+  title: string;
+}
 
 const Home: React.FC = () => {
+  const [popularMovies, setPopularMovies] = useState<PopularMovie[]>([]);
+
+  useEffect(() => {
+    api
+      .get(`movie/popular`, {
+        params: {
+          api_key: PRIVATE_KEY,
+          language: 'en-US',
+          page: 1,
+        },
+      })
+      .then(({ data, status }) => {
+        const { results } = data;
+
+        if (status === 200) {
+          setPopularMovies(results);
+        }
+      });
+  }, []);
+
   return (
     <>
       <Header />
-
       <Container>
-        <Content>
-          <DataList />
-        </Content>
+        <Main>
+          <Content>
+            <Section>
+              <DataList popularMovies={popularMovies} />
+            </Section>
+          </Content>
+        </Main>
       </Container>
     </>
   );
