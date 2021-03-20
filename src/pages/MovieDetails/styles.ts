8adcccf95c6e2a67a64dd281905e4a7f78887ad2
José <josejustinno@gmail.com>
2021-03-20 15:39:19 -0300

    finish movie details css style

--- src/pages/MovieDetails/styles.ts
+++ src/pages/MovieDetails/styles.ts
@@ -19,9 +19,13 @@ export const Content = styled.div`
   overflow-y: auto;
   right: 0;
   bottom: 0;
+
+  width: 100%;
 `;
 
 export const ContentBackground = styled.div<ContentProps>`
+  height: 100%;
+
   ${props =>
     props.image &&
     css`
@@ -31,12 +35,11 @@ export const ContentBackground = styled.div<ContentProps>`
     `}
 `;
 
-export const ContainerSections = styled.div`
-  padding: 1.25rem;
-  margin-top: 1rem;
-
+export const ContainerInfo = styled.div`
   display: flex;
-  flex-direction: row;
+  justify-content: center;
+
+  height: 100%;
 
   background-image: linear-gradient(
     to right,
@@ -45,12 +48,21 @@ export const ContainerSections = styled.div`
   );
 `;
 
+export const ContainerSections = styled.div`
+  padding: 1.25rem;
+  margin-top: 1rem;
+
+  max-width: 1440px;
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+`;
+
 export const ImageSection = styled.section`
   padding: 1.75rem;
 
   & > img {
-    width: 20rem;
-    height: 30rem;
     border-radius: 10px;
   }
 `;
@@ -59,23 +71,77 @@ export const ContentSection = styled.section`
   padding: 1.25rem;
   margin-top: 1.75rem;
 
+  h2 {
+    font-size: 2rem;
+  }
+
   & > p.quick-info {
     display: flex;
     flex-direction: row;
 
-    margin-top: 1rem;
+    margin-top: 0.25rem;
+
+    span.genres,
+    span.duration {
+      position: relative;
+      margin-left: 1rem;
+
+      display: inline-flex;
+      align-content: center;
+      align-items: center;
+    }
+
+    span.genres::before,
+    span.duration::before {
+      content: '';
+
+      background-color: var(--white);
+
+      width: 0.25rem;
+      height: 0.25rem;
+      border-radius: 50%;
+      left: -0.5rem;
+
+      position: absolute;
+    }
   }
 
   & > div.user-score {
     margin-top: 1rem;
   }
 
-  p {
-    margin-top: 1rem;
+  h3 {
+    margin-top: 0.75rem;
+  }
+
+  p.abstract {
+    margin-top: 0.75rem;
   }
 
   & > div.authors {
     display: flex;
     flex-direction: row;
+    justify-content: flex-start;
+
+    margin-top: 1.5rem;
+
+    ol {
+      list-style: none;
+
+      width: 33%;
+
+      li + li {
+        margin-top: 1.5rem;
+      }
+
+      li {
+        width: 100%;
+        padding-right: 1.5rem;
+
+        p:last-child {
+          font-size: 0.75rem;
+        }
+      }
+    }
   }
 `;
